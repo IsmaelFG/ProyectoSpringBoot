d947e21c6faf5ad3348b85944a98e1905ac692af
IsmaelFG <ismael.fergar04@gmail.com>
2024-06-09 18:57:52 +0200

    Empezado desarrollo del CRUD mostrar la tabla usuario

--- src/main/java/com/example/demo/controller/EntradaController.java
+++ src/main/java/com/example/demo/controller/EntradaController.java
@@ -1,13 +1,21 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.repository.UsuarioRepository;
+
 
 @Controller
 public class EntradaController {
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 
 	@GetMapping("/")
-	public String index() {
+	public String indexMostrarUsuarios(Model model) {
+		model.addAttribute("usuarios", usuarioRepository.findAll());
 		return "index";
 	}
 }
--- src/main/resources/static/sql/script.sql
+++ src/main/resources/static/sql/script.sql
@@ -10,3 +10,15 @@ CREATE TABLE usuarios (
     fecha_alta DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     fecha_baja DATETIME DEFAULT NULL
 );
+
+INSERT INTO usuarios (usuario, clave, nombre, apellido1, apellido2, edad, direccion, fecha_alta, fecha_baja) VALUES
+('usuario1', 'clave1', 'Nombre1', 'Apellido1-1', 'Apellido1-2', 30, 'Direccion1', NOW(), NULL),
+('usuario2', 'clave2', 'Nombre2', 'Apellido2-1', 'Apellido2-2', 25, 'Direccion2', NOW(), NULL),
+('usuario3', 'clave3', 'Nombre3', 'Apellido3-1', 'Apellido3-2', 40, 'Direccion3', NOW(), NULL),
+('usuario4', 'clave4', 'Nombre4', 'Apellido4-1', 'Apellido4-2', 35, 'Direccion4', NOW(), NULL),
+('usuario5', 'clave5', 'Nombre5', 'Apellido5-1', 'Apellido5-2', 28, 'Direccion5', NOW(), NULL),
+('usuario6', 'clave6', 'Nombre6', 'Apellido6-1', 'Apellido6-2', 32, 'Direccion6', NOW(), NULL),
+('usuario7', 'clave7', 'Nombre7', 'Apellido7-1', 'Apellido7-2', 29, 'Direccion7', NOW(), NULL),
+('usuario8', 'clave8', 'Nombre8', 'Apellido8-1', 'Apellido8-2', 27, 'Direccion8', NOW(), NULL),
+('usuario9', 'clave9', 'Nombre9', 'Apellido9-1', 'Apellido9-2', 33, 'Direccion9', NOW(), NULL),
+('usuario10', 'clave10', 'Nombre10', 'Apellido10-1', 'Apellido10-2', 26, 'Direccion10', NOW(), NULL);
